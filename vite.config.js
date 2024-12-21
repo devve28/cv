9a94073f0050a3234c155bdcdcc3536aaa1da55b
Eugene Baranov <jekaizurupinska@gmail.com>
2024-12-22 02:35:09 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://devve28.github.io/cv/'
+  base: 'https://devve28.github.io/cv'
 })
